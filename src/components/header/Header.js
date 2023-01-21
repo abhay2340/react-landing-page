9ab3d44640cc8893dc1cbdaaee4760199e198b86
abhay2340 <abhayverma5051@gmail.com>
2023-01-21 17:48:34 +0530

    added mobile resposiveness

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import './Header.css'
 import image from '../../images/image-hero-desktop.png'
+import imgone from '../../images/client-databiz.svg'
+import imgtwo from '../../images/client-maker.svg'
+import imgthree from '../../images/client-audiophile.svg'
+import imgfour from '../../images/client-meet.svg'
 
 function Header() {
   return (
     <div className='header'>
     <div className='left'>
-      <h1>Make remote wrok</h1>
+      <h1>Make <br/> remote wrok</h1>
       <p>navigable address as the href value. If you cannot provi
 de a valid href, but still need the element to resemble a link, use a butt
 on and change it with appropriate styles. 
       </p>
+      <button type='button'>Learn more</button>
+      <div className='sponsers'>
+      <img src={imgone}></img>
+      <img src={imgtwo}></img>
+      <img src={imgthree}></img>
+      <img src={imgfour}></img>
+      </div>
     </div>
     <div className='right'><img src={image}></img></div>
 
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,19 +2,34 @@ import {useState} from 'react'
 import './Navbar.css'
 import logo from '../../images/logo.svg'
 import { TfiAngleDoubleDown,TfiAngleDoubleLeft } from "react-icons/tfi";
+import { FiChevronDown,FiChevronUp } from "react-icons/fi";
 // import { FaBeer } from 'react-icons/fa';
 
 function Navbar() {
   const [toggle,setToggle]=useState(false)
+  const [togglecomp,setToggleComp]=useState(false)
+  const [togglecar,setToggleCar]=useState(false)
   return (
     <>
     <div className='navbar'>
         <img src={logo} id="logo"></img>
       <div className='navbar-links'>
         <p><a href='#'>Features</a></p>
-        <p><a href='#'>Comaony</a></p>
-        <p><a href='#'>Careers</a></p>
-        <p><a href='#'>About</a></p>
+        <p><a href='#'>Company</a> <p>{togglecomp?<FiChevronUp className='up' onClick={()=>{setToggleComp(false)}}/>:<FiChevronDown onClick={()=>{setToggleComp(true)}}/>} {togglecomp&&(
+        <div className='company-dropdown'>
+          <p>HOME</p>
+          <p>HOME</p>
+          <p>HOME</p>
+          </div>
+        )}</p></p>
+        <p><a href='#'>Careers</a> <p>{togglecar?<FiChevronUp className='up' onClick={()=>{setToggleCar(false)}}/>:<FiChevronDown onClick={()=>{setToggleCar(true)}}/>} {togglecar&&(
+        <div className='car-dropdown'>
+          <p>HOME</p>
+          <p>HOME</p>
+          <p>HOME</p>
+          </div>
+        )}</p></p>
+        <p><a href='#'>About</a> </p>
       </div>
       <div className='signin-toggle'>
       <div className='navbar-signin'>
